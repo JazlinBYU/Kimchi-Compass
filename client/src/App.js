fe6534db597f806a578bf794d0c587b7baaf1a78
Jazlin Yu <jazlinsub@gmail.com>
2023-12-13 21:34:01 -0800

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import { UserProvider } from "./UserContext";
 import Home from "./components/Home";
@@ -7,23 +7,26 @@ import SignUp from "./components/SignUp";
 import RestaurantDetails from "./components/RestaurantDetails";
 import UserProfile from "./components/UserProfile";
 import AddRestaurantForm from "./components/AddRestaurantForm";
+// import ErrorPage from "./components/ErrorPage"; // Assuming you have an ErrorPage component
 
 function App() {
   return (
     <UserProvider>
       <SnackbarProvider maxSnack={3}>
-        {" "}
-        {/* Wrap with SnackbarProvider */}
-        <div>
-          {/* Navigation Bar and other shared components */}
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/register" component={SignUp} />
-            <Route path="/restaurants/:id" component={RestaurantDetails} />
-            <Route path="/profile" component={UserProfile} />
-            <Route path="/add-restaurant" component={AddRestaurantForm} />
-          </Routes>
-        </div>
+        <Router>
+          <div>
+            {/* Navigation Bar and other shared components */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/restaurants/:id" element={<RestaurantDetails />} />
+              <Route path="/profile" element={<UserProfile />} />
+              <Route path="/add-restaurant" element={<AddRestaurantForm />} />
+              {/* <Route path="*" element={<ErrorPage />} />{" "} */}
+              {/* Handle unmatched routes */}
+            </Routes>
+          </div>
+        </Router>
       </SnackbarProvider>
     </UserProvider>
   );
